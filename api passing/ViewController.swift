c4e224513f8f2beb7e45ad2209e8b37851f11ce2
Manahil Fatima <125747158+manahil-gillani@users.noreply.github.com>
2024-07-28 23:36:37 +0500

    Initial Commit

---
+++ api passing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  api passing
+//
+//  Created by syeda manahil fatima on 28/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
